5f62588ef04b775aada5d28af758817fe4f420ed
yoshiya0503 <myon53@gmail.com>
2016-04-25 08:20:09 +0900

    [components] add header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,63 @@
+/**
+ * @fileoverview Header_UI
+ * @name Header.jsx
+ * @author Yoshiya Ito <ito_yoshiya@cyberagnet.co.jp>
+ */
 import React, { PropTypes, Component } from 'react';
-import mui, { AppBar } from 'material-ui';
-const ThemeManager = require('material-ui/lib/styles/theme-manager');
+import mui, {
+    AppBar,
+    Tabs,
+    Tab
+} from 'material-ui';
 
-class Header extends Component {
-    static get childContextTypes() {
-        return { muiTheme: React.PropTypes.object };
+/**
+ * @class Menu
+ * @extends Component
+ * @description メニュー一覧
+ */
+class Menu extends Component {
+    /**
+     * @method render
+     * @return メニュー一覧
+     */
+    render() {
+        return (
+            <Tabs>
+              <Tab
+                label='SERVICE'
+              />
+              <Tab
+                label='PRODUCT'
+              />
+              <Tab
+                label='ABOUT'
+              />
+                <Tab
+              label='CONTACT'
+              />
+            </Tabs>
+        )
     }
+}
 
+/**
+ * @class Header
+ * @extends Component
+ * @description ヘッダ情報, コーポレートサイトのタブ
+ */
+class Header extends Component {
+    /**
+     * @method render
+     * @return ヘッダ情報
+     */
     render() {
         return (
             <header className='header'>
-              <h1>AppBar Component</h1>
-              <AppBar title='this is test' />
+              <AppBar
+                title = { 'FunnyFace, inc' }
+                showMenuIconButton = { false }
+                children = { <Menu/> }
+              />
             </header>
         )
     }
--- components/MainSection.jsx
+++ components/MainSection.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import mui, {CircularProgress,
+import mui, {
+    CircularProgress,
     Tabs,
     Tab,
     DatePicker
