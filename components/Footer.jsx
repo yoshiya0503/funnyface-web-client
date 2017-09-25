8817302c4d9fe434b01747df7615d3b12e8caebd
yoshiya0503 <myon53@gmail.com>
2017-09-25 17:15:28 +0900

    fix

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Footer_UI
+ * @name Footer.jsx
+ * @author Yoshiya Ito <ito_yoshiya@cyberagnet.co.jp>
+ */
+import React, { PropTypes, Component } from 'react';
+import mui, {
+}
